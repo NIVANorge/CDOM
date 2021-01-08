30521024a8646866d3218ff7570f0902e5e84463
Therese Harvey <therese.harvey@niva-dk.dk>
2021-01-08 13:11:09 +0100

    Script for chnaging sp to text and sort out redundant lines

--- sp_to_text.R
+++ sp_to_text.R
@@ -7,11 +7,11 @@ source("read_sp.R")
 
 folder_in <- "example" # change to correct folder
 #e.g
-#folderin<-"K:/Avdeling/214-Oseanografi/_LAB/CDOM/Data/190510"
+folder_in<-"K:/Avdeling/214-Oseanografi/_LAB/CDOM/Data/190510"
 
 folder_out <- "example_out" # create a new folder where the txt files should be placed (e.g. within the raw data folder)
 
-#folder_out<-"K:/Avdeling/214-Oseanografi/_LAB/CDOM/Data/190510/txt"
+folder_out<-"K:/Avdeling/214-Oseanografi/_LAB/CDOM/Data/190510/txt"
 
 filelist<-list.files(path = folder_in, pattern = "*.sp", ignore.case = T, full.names = F)
 
@@ -24,5 +24,6 @@ for(filename in filelist){
   fileout<-paste0(folder_out,"/",substr(filename,1,nchar(filename)-3),".txt")
   write.table(df, file = fileout, sep = "\t", row.names = F, col.names = T, quote = F)
 }
+
 
 
